cf4ce1204b2f62c9ad9d369ec2ce7d388f2caa5c
OscarRuiz6007 <oscarruiz35@yahoo.com>
2025-11-18 22:39:36 -0500

    Update sourceCode.m
    Made a new branch, just so that I have a backup if the code I just did doesn't work for any reason, I'll check it myself beforehand anyways, It's mostly just reused stuff from the one time I used the buzzer. If it does all work then the project should be done.

--- sourceCode.m
+++ sourceCode.m
@@ -7,27 +7,31 @@ clear, clc, close all;
 a = arduino('COM3', 'Uno'); % Make an arduino object to use it
 
 % Variables
-i = 0; % for looping
+i = 1; % for looping
 Time = []; % Create a vector to store time data
 Voltage = []; % Create a vector to store data of read voltage
 X_LIMIT = 100; % Default plot x-axis range
-Y_LIMIT = 1; % Default plot y-axis range
+Y_LIMIT = 7; % Default plot y-axis range
 on = true; % Default boolean value that automatically starts program
+ohms = 1000;  % The expected ohms used in the project
+DEFAULT_PAUSE_TIME = .1; % Default pause time for verification noise
+BUZZER_VOLTAGE_CHECK = 1; % Default variable for the amount of times buzzer is activated
 
 % Plot Initiation
 figure; % create a plot area
 xlim([0 X_LIMIT]);%set limit for plot
 ylim([0 Y_LIMIT]);%set limit for plot
-title('Ultrasonic Distance Measurements Collected'); % Add a Title
+title('Voltage Measurements Collected'); % Add a Title
 xlabel('Time (Time) [s]'); % Add Horizontal Axis Labeling
-ylabel('Distance (Measurement) [m]'); % Add Vertical Axis Labeling
+ylabel('Voltage (Voltage) [m]'); % Add Vertical Axis Labeling
 grid on; % Add Grid Lines
 hold on;
 
-% Active Data Collection to be Graphed
-while on == true % Start of loop for data collection
-
-    if i == 0
+% Active Data Collection
+while on == true % start of loop
+    
+    % Start of loop setup
+    if i == 1
     disp("Start") % Notiy user of Start 
     pause(2); % Starting period grace
     tic(); % Begins a clock to keep track of time
@@ -39,14 +43,34 @@ while on == true % Start of loop for data collection
     % Graphical updates
     Time(i) = toc(); % Save the time elasped in seconds for each measurement
     Voltage(i) = readVoltage(a, 'A0'); % Collect/Save the Measurement Data
-    plot(Time(i), Measurement(i), 'b*') % Point Plotting
+    plot(Time(i), Voltage(i), 'b*') % Point Plotting
     drawnow % Making point plotting live
 
-    if i >= 100 % program shutdown after 100 loops
+    % Power Calculation
+    power(i) = voltage(i) * (voltage(i)/ohms);
+
+    % Display requirement
+    if Voltage(i) >= 5 % conditional statement for buzzer turning on and off
+        playTone(a, 'D3', 0, 1); % buzz off
+        BUZZER_VOLTAGE_CHECK = 1; % reset the buzzer speed
+    elseif Measurement(i) <= 0.3 % conditional statement for buzzer turning off
+        playTone(a, 'D3', 900 + BUZZER_VOLTAGE_CHECK * 50, 1); % buzz on
+        pause(DEFAULT_PAUSE_TIME / BUZZER_VOLTAGE_CHECK);
+        playTone(a, 'D3', 0, 1); % buzz off
+        pause(DEFAULT_PAUSE_TIME / BUZZER_VOLTAGE_CHECK);
+        BUZZER_VOLTAGE_CHECK = BUZZER_VOLTAGE_CHECK + .25;
+    end
+
+    % Program shutdown after 100 loops
+    if i >= 100 
         on = false;
     end
-    
     i = i + 1; % adds to loop counter i
+
+    % Text display to user
+    disp("Current Loop Iteration: " + i)
+    disp("Current Voltage Read for Current Loop " + Voltage(i))
+    disp("Power Calculated for Current Loop: " + power(i))
 end
 
-disp("end") % notify user that the program has ended
+disp("end") % tells user program has endedr that the program has ended
